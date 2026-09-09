7f2e7dd978c81d2acd937a5d5828edc61cf16277
klya08 <kalyawahyu94@gmail.com>
2026-09-09 14:16:32 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -293,41 +293,45 @@ if jumlah_sekarang < 5:
     )
 
     webrtc_ctx = webrtc_streamer(
-        key=f"kamera_{st.session_state.kamera_key}",
-        mode=WebRtcMode.SENDRECV,
+    key=f"kamera_{st.session_state.kamera_key}",
+    mode=WebRtcMode.SENDRECV,
 
-        media_stream_constraints={
-            "video": {
-                "facingMode": {
-                    "ideal": "environment"
-                }
-            },
-            "audio": False
-        },
-
-        rtc_configuration={
-            "iceServers": [
-                {
-                    "urls": [
-                        "stun:stun.l.google.com:19302"
-                    ]
-                }
-            ]
-        },
-
-        video_html_attrs={
-            "autoPlay": True,
-            "controls": False,
-            "style": {
-                "width": "100%",
-                "height": "70vh",
-                "object-fit": "cover",
-                "border-radius": "12px"
+    media_stream_constraints={
+        "video": {
+            "facingMode": {
+                "ideal": "environment"
             }
         },
+        "audio": False
+    },
 
-        media_toggle_controls=False
-    )
+    rtc_configuration={
+        "iceServers": [
+            {
+                "urls": [
+                    "stun:stun.l.google.com:19302"
+                ]
+            }
+        ]
+    },
+
+    video_html_attrs={
+        "autoPlay": True,
+        "controls": False,
+        "muted": True,
+        "playsInline": True,
+        "style": {
+            "width": "100%",
+            "height": "70vh",
+            "min-height": "500px",
+            "object-fit": "cover",
+            "border-radius": "12px",
+            "display": "block"
+        }
+    },
+
+    media_toggle_controls=False
+)
 
 else:
     st.warning(
